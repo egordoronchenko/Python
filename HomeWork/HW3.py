3cb2451bfe808a979b35598c3764b63183390522
egordoronchenko <138254184+egordoronchenko@users.noreply.github.com>
2023-09-26 21:40:05 +0300

    решение скомуниздил но вроде как понял механику

---
+++ HomeWork/HW3.py
@@ -0,0 +1,18 @@
+list_letters = {1: "AEIOULNSTRАВЕИНОРСТ",
+                2: "DGДКЛМПУ",
+                3: "BCMPБГЁЬЯ",
+                4: "FHVWYЙЫ",
+                5: "KЖЗХЦЧ",
+                8: "JXШЭЮ",
+                10: "QZФЩЪ"}
+
+word = input("Введите слово: ").upper()
+summ = 0
+for i in word:
+    for k, v in list_letters.items():
+        if i in v:
+            summ += k
+print(f"Стоимость слова: {summ}")
+
+
+
