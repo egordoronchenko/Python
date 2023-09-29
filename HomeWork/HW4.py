0f259e9ef36c84ee8bedfdf0271e7faecf57b80c
egordoronchenko <138254184+egordoronchenko@users.noreply.github.com>
2023-09-29 20:20:56 +0300

    Подкорректировал задачу в ответ на замечание

--- HomeWork/HW4.py
+++ HomeWork/HW4.py
@@ -6,13 +6,13 @@
 2 4 6 8 10 12 10 8 6 4 2
 3 6 9 12 15 18
 6 12'''
-from random import randint
-n_set = set(randint(1, 10) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-print(n_set)
-m_set = set(randint(1, 10) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-print(m_set)
-s_set = sorted(n_set.intersection(m_set))
-print(*s_set)
+# from random import randint
+# n_set = set(randint(1, 10) for i in range(int(input('Введите кол-во элементов первого множества: '))))
+# print(n_set)
+# m_set = set(randint(1, 10) for i in range(int(input('Введите кол-во элементов второго множества: '))))
+# print(m_set)
+# s_set = sorted(n_set.intersection(m_set))
+# print(*s_set)
 
 
 '''Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. Она растет на круглой грядке,
@@ -28,16 +28,21 @@ print(*s_set)
 находясь перед некоторым кустом заданной во входном файле грядки.
 4 -> 1 2 3 4
 9'''
-# n = 5 # кустов
-# from random import randint
-# list_1 = list(randint(1, 5) for i in range(int(input('Введите кол-во кустов: '))))
-# print(list_1)
-# a = int(input('Введите № куста: '))
-# res = 0
-# if a == 1:
-#     res = list_1[0] + list_1[1] + list_1[-1]
-# elif a == len(list_1):
-#     res = list_1[-2] + list_1[-1] + list_1[0]
-# else:
-#     res = list_1[a-1] + list_1[a-2] + list_1[a]
-# print(res, 'ягод')
+
+file = open("text-files/vvodnie.txt")
+a = int(file.read(1))
+file.close()
+
+n = 5 # кустов
+from random import randint
+list_1 = list(randint(1, 5) for i in range(int(input('Введите кол-во кустов: '))))
+print(list_1)
+
+res = 0
+if a == 1:
+    res = list_1[0] + list_1[1] + list_1[-1]
+elif a == len(list_1):
+    res = list_1[-2] + list_1[-1] + list_1[0]
+else:
+    res = list_1[a-1] + list_1[a-2] + list_1[a]
+print(res, 'ягод')
