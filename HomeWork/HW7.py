1fb2fcb5456ca7b332e3bdeede245677dee80059
egordoronchenko <138254184+egordoronchenko@users.noreply.github.com>
2023-10-03 21:26:04 +0300

    добавлен 7ой семинар и домашняя работа к нему

--- HomeWork/HW7.py
+++ HomeWork/HW7.py
@@ -11,9 +11,22 @@
 # else:
 #     print('Пам парам')
 
+# def print_operation_table(operation, num_rows=6, num_columns=8):
+#     res = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in res:
+#         print(*[f"{j}" for j in i])
+#
+# print_operation_table(lambda x, y: x * y, 3, 3)
+import sys
+
+
 def print_operation_table(operation, num_rows=6, num_columns=8):
+    if num_rows < 2:
+        print("ОШИБКА! Размерности таблицы должны быть больше 2!")
+        sys.exit()
     res = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in res:
         print(*[f"{j}" for j in i])
 
-print_operation_table(lambda x, y: x * y)
+
+print_operation_table(lambda x, y: x * y, 1, 3)
